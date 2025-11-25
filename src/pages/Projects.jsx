605e6ea6a7da1619923f0c885ed72dd75db58b76
Tanmay Warthe <tanmaywarthe27@gmail.com>
2025-11-26 02:11:09 +0530

    fix the issues

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Projects() {
+    return (
+        <section id="projects" className="py-2 sm:py-3">
+            <div className="max-w-6xl mx-auto px-4 sm:px-6">
+                <h2 className="text-3xl font-bold text-gray-900 mb-6">Projects</h2>
+                <p className="text-lg text-gray-600">
+                    {/* TODO: Add project cards or showcase here */}
+                    This section will showcase my projects.
+                </p>
+            </div>
+        </section>
+    );
+}
